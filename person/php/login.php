686b58081f74a208f5adc52689852b15c8f281bd
赵开 <zhkai0530@sina.com>
2019-03-07 22:37:22 +0800

    更改登录判断，使用mysqli_num_rows($result)<=0来判断获取的结果集是否为空，从而判断登录名是否存在

--- person/php/login.php
+++ person/php/login.php
@@ -2,7 +2,6 @@
 	class Login {
 		public $user;
 		public $pwd;
-		public $is_user_logged_in = 0;
 		function __construct(){
 			require 'config.php';
 			$this->user = $_POST["name"];
@@ -13,7 +12,7 @@
 			if(!trim($this->pwd)==""){
 				$strlen = strlen($this->pwd);
 				if($strlen>20 || $strlen<6){
-					echo "<script>alert('您的密码长度错误，请再试一次。')</script>";
+					echo "<script>alert('您的密码长度错误，请再试一次。');history.go(-1);</script>";
 					exit();
 				}else{
 					$this->pwd = md5($this->pwd);
@@ -24,26 +23,33 @@
 		function checkuser (){
 			$conn = new mysqli(DB_HOST,DB_USER,DB_PWD,DB_NAME) or die("数据库连接失败");
 			mysqli_query($conn,"set names utf8");
-			$sql = "select * from personinfo where name='".$this->user."' and password='".$this->pwd."'";
-			$result = mysqli_fetch_row($conn->query($sql))[0];	
-			if(!$result){
-				echo "<script>alert('Email or password is incorrect.please try again!');history.go(-1);</script>";
-			exit();
-			}else{
-				$conn->close();
-				$_SESSION['user'] = $result;
-				echo "<script>alert('登录成功!');location.href = 'http://127.0.0.1/tc/index.php'</script>";
+			$sql = "select * from personinfo where phone='".$this->user."' or email='".$this->user."'";
+			$result = mysqli_query($conn,$sql);
+			if(!$result)
+			{
+				echo "<script>alert(\"用户名错误\");history.go(-1);</script>";
 				exit();
+			}else{
+				if(mysqli_num_rows($result) <= 0){
+					echo "<script>alert(\"用户名错误\");history.go(-1);</script>";
+					exit();
+				}else{
+					while ($row = mysqli_fetch_array($result)) 
+					{
+						if($row['phone'] === $this->user or $row['email'] === $this->user)
+						{
+							if($row["password"] === $this->pwd)
+							{
+								echo $row['name'];
+							}else{
+								echo "<script>alert(\"密码错误\");history.go(-1);</script>";
+								exit();
+							}
+						}
+					}
+				}
 			}
-			// while($row = mysqli_fetch_array($result)){
-				// if($row['pwd']===$this->pwd && $row['name'] === $this->user){
-				// 	echo "登录成功";
-				// }else{
-				// 	echo "用户名或密码错误";
-				// }
-			// }		
 		}
-
 		function doLogin(){
 			$this->checkpwd();
 			$this->checkuser();
