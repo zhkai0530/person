31ca3c38366c1460784f5cace4a8db0261824bba
赵开 <zhkai0530@sina.com>
2019-03-12 22:01:24 +0800

    添加页面滚动大于40后，导航栏固定页面顶端

--- person/js/head.js
+++ person/js/head.js
@@ -5,6 +5,11 @@ window.onload = function(){
 	var urlPath = window.location.pathname;
 	var arrUrl = urlPath.split("/");
 	var pageName = arrUrl[arrUrl.length-1];
+	var reg = document.getElementById("reg");
+	var logIn = document.getElementById("login");
+	var logInBTN = document.getElementsByClassName("logInBTN")[0];
+	var registerBTN = document.getElementsByClassName("registerBTN")[0];
+	var close = document.getElementsByClassName("close");
 	for(var i = 0;i<navLi.length;i++){
 		navLi_a[i] = navLi[i].getElementsByTagName("a");
 		var aHref = navLi_a[i][0].href.split("/");
@@ -12,4 +17,19 @@ window.onload = function(){
 			navLi_a[i][0].style.color = "#aeaeae";
 		}
 	}
+
+	//监听页面滚动，导航栏置顶
+	var navBar = document.getElementsByClassName("navBar");
+	window.onscroll = function(){
+		var scroll_top = document.documentElement.scrollTop || document.body.scrollTop;
+		if(scroll_top>=40){
+			navBar[0].style.cssText = "position:fixed;top:0px;z-index:9";
+		}else{
+			navBar[0].style.cssText = "position:normal";
+		}
+	}
+	var e = window || window.lib;
+	var r = e.document;
+	var a = r.documentElement;
+	console.log(a)
 }
