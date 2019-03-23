5dc14585aef51d38b1c615adc86e4828aaeb0131
赵开 <zhkai0530@sina.com>
2019-03-23 15:31:48 +0800

    整理JS 代码，使其更有可读性；添加显示和隐藏登录和注册页面的功能；通过添加class的方法实现显示和隐藏登录和注册页面的动效。

--- person/js/head.js
+++ person/js/head.js
@@ -1,35 +1,57 @@
 window.onload = function(){
-	var navBar = document.getElementsByTagName("nav");
-	var navLi = navBar[0].getElementsByTagName("li");
-	var navLi_a = new Array();
-	var urlPath = window.location.pathname;
-	var arrUrl = urlPath.split("/");
-	var pageName = arrUrl[arrUrl.length-1];
-	var reg = document.getElementById("reg");
-	var logIn = document.getElementById("login");
-	var logInBTN = document.getElementsByClassName("logInBTN")[0];
-	var registerBTN = document.getElementsByClassName("registerBTN")[0];
-	var close = document.getElementsByClassName("close");
-	for(var i = 0;i<navLi.length;i++){
-		navLi_a[i] = navLi[i].getElementsByTagName("a");
-		var aHref = navLi_a[i][0].href.split("/");
-		if(aHref[aHref.length-1] == pageName){
-			navLi_a[i][0].style.color = "#aeaeae";
-		}
-	}
-
 	//监听页面滚动，导航栏置顶
-	var navBar = document.getElementsByClassName("navBar");
-	window.onscroll = function(){
-		var scroll_top = document.documentElement.scrollTop || document.body.scrollTop;
-		if(scroll_top>=40){
-			navBar[0].style.cssText = "position:fixed;top:0px;z-index:9";
-		}else{
-			navBar[0].style.cssText = "position:normal";
+	(function(){
+		var navBar = document.getElementsByClassName("navBar");
+		window.onscroll = function(){
+			var scroll_top = document.documentElement.scrollTop || document.body.scrollTop;
+			if(scroll_top>=40){
+				navBar[0].style.cssText = "position:fixed;top:0px;z-index:9";
+			}else{
+				navBar[0].style.cssText = "position:normal";
+			}
 		}
-	}
-	var e = window || window.lib;
-	var r = e.document;
-	var a = r.documentElement;
-	console.log(a)
+	})();
+
+	//不同页面的导航显示不同样式
+	(function(){
+		var url = window.location.pathname.split('?')[0].split('/').pop(),
+			links = document.querySelector('nav').getElementsByTagName('ul')[0].getElementsByTagName('a'),
+			index = 0;
+		if(url){
+			for(var i = 0;i<url.length;i++){
+				if(links[i].href.indexOf(url) !== -1){
+					index = i;
+					break;
+				}
+			}
+		}
+		links[index].style.color = "#aeaeae";
+	})();
+
+	//显示/隐藏登录和注册页面
+	(function(){
+		var	close = document.getElementsByClassName('close'),
+			formData = document.getElementsByClassName('formCtr'),
+			formBTN = document.getElementsByClassName('formBTN');
+			for(var i = 0;i<formBTN.length;i++){
+				(function(i){
+					formBTN[i].onclick = function(){
+						formData[i].style.display = "block";
+						formData[i].classList.add("movedown");
+					}
+				})(i)
+			}
+			for(var j = 0;j<close.length;j++){
+				(function(j){
+					close[j].onclick = function(){
+						formData[j].classList.remove("movedown");
+						formData[j].classList.add("moveup");
+						var time = setTimeout(function(){
+							formData[j].classList.remove("moveup");
+							formData[j].style.display = "none";
+						}, 500);
+					}
+				})(j)
+			}
+	})()
 }
