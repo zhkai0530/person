8dd96fd263e027d9f21958528a3cc56e2040e6d9
赵开 <zhkai0530@sina.com>
2019-04-24 23:12:14 +0800

    博客入口文件

--- person/blog.php
+++ person/blog.php
@@ -1,18 +1,9 @@
 <!DOCTYPE html>
 <html lang="cn">
-<head>
-	<meta charset="UTF-8">
-	<title>博客</title>
-	<link rel="stylesheet" type="text/css" href="css/head.css">
-	<link rel="stylesheet" type="text/css" href="css/leftsidebar.css">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<link rel="stylesheet" type="text/css" href="css/blog.css">
-	<link rel="stylesheet" type="text/css" href="css/footer.css">
-	<script type="text/javascript" src="js/head.js"></script>
-</head>
+<?php require_once("Template/head.php") ?>
 <body>
-	<?php include "Template/head.php"; ?>
-	<div class="contentBar clearfix">
+	<?php include "Template/header.php"; ?>
+	<div class="indexContentBar clearfix">
 		<?php 
 			include "Template/leftsidebar.php"; 
 			include "Template/container.php";
